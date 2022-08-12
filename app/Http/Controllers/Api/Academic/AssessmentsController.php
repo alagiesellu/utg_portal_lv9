1951353aa8692f6937127c7a029a0370cf9f3fd4
Alagie Sellu <alagiesellu@gmail.com>
2022-08-12 11:48:46 +0000

    Minor Log import error.

--- app/Http/Controllers/Api/Academic/AssessmentsController.php
+++ app/Http/Controllers/Api/Academic/AssessmentsController.php
@@ -9,6 +9,7 @@ use App\Models\Academics\TermClass;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 
 class AssessmentsController extends Controller
